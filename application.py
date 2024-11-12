e3b7e348c50acfa4253834eb85c04cd956825199
ikenna ughanze <ughanzepolycarp@gmail.com>
2024-11-12 12:36:55 +0000

    edit app.py

--- application.py
+++ application.py
@@ -1,5 +1,6 @@
 from website import create_app
 
+application = create_app()  # Move this outside of the `if __name__ == "__main__"` block
+
 if __name__ == "__main__":
-    application = create_app()
     application.run(debug=True)
